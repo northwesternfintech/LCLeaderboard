c36f88fbfa54c92cae57de28afe1399abf455dd9
Jason <jasonchicago@att.net>
2022-08-03 13:08:30 -0500

    Removed title refactor and created basic layout with mapping

--- lc-leaderboard/pages/index.js
+++ lc-leaderboard/pages/index.js
@@ -4,86 +4,137 @@ import styles from '../styles/Home.module.css'
 import react from 'react';
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 import { SpeakerphoneIcon, XIcon } from '@heroicons/react/outline'
-import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon } from '@heroicons/react/outline'
-import Title from './Title/Title'
+import { AnnotationIcon, StarIcon, LightningBoltIcon, PlusIcon} from '@heroicons/react/outline'
+
 
 function getKeyByValue(object, value) {
   return Object.keys(object).find(key => object[key] === value);
 }
 
-const features = [
-  {
-    name: 'Competitive exchange rates',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: GlobeAltIcon,
-  },
-  {
-    name: 'No hidden fees',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: ScaleIcon,
-  },
-  {
-    name: 'Transfers are instant',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: LightningBoltIcon,
-  },
-  {
-    name: 'Mobile notifications',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
-    icon: AnnotationIcon,
-  },
-]
 
-export default function Home({leaderboard, array}) {
+
+
+export default function Home({leaderboard, array, easies}) {
   array.sort(function(a,b){return a - b})
   array.reverse()
   console.log(leaderboard)
   console.log("Winner! " + getKeyByValue(leaderboard, array[0]) + " Complete: " + array[0]);
   console.log("Second place! " + getKeyByValue(leaderboard, array[1]));
+  console.log(easies)
 
   let winner = getKeyByValue(leaderboard, array[0]);
 
+  const ranks = [
+    {
+      place: "#1",
+      name: getKeyByValue(leaderboard, array[0]),
+      description:
+        'GrindLord',
+      link:"https://leetcode.com/" + getKeyByValue(leaderboard, array[0]),
+      count : array[0],
+      icon: StarIcon,
+    },
+    {
+      place: "#2",
+      name: getKeyByValue(leaderboard, array[1]),
+      description:
+        'Up and coming',
+      link:"https://leetcode.com/" + getKeyByValue(leaderboard, array[1]),
+      count : array[1],
+      icon: LightningBoltIcon,
+    },
+    {
+      place: "#3",
+      name: getKeyByValue(leaderboard, array[2]),
+      description:
+        'Not Bad',
+      link:"https://leetcode.com/" + getKeyByValue(leaderboard, array[2]),
+      count : array[2],
+      icon: PlusIcon,
+    },
+    {
+      place: "#4",
+      name: getKeyByValue(leaderboard, array[3]),
+      description:
+        'Almost there',
+      link:"https://leetcode.com/" + getKeyByValue(leaderboard, array[3]),
+      count : array[3],
+      icon: PlusIcon,
+    },
+    {
+      place: "#5",
+      name: getKeyByValue(leaderboard, array[4]),
+      description:
+        'Start Grinding Bro',
+      link:"https://leetcode.com/" + getKeyByValue(leaderboard, array[4]),
+      count : array[4],
+      icon: PlusIcon,
+    },
+    {
+      place: "#6",
+      name: getKeyByValue(leaderboard, array[5]),
+      description:
+        'Step it up',
+      link:"https://leetcode.com/" + getKeyByValue(leaderboard, array[5]),
+      count : array[5],
+      icon: PlusIcon,
+    },
+    {
+      place: "#7",
+      name: getKeyByValue(leaderboard, array[6]),
+      description:
+        'Are you even leetcoding??',
+      link:"https://leetcode.com/" + getKeyByValue(leaderboard, array[6]),
+      count : array[6],
+      icon: PlusIcon,
+    }
+  ]
+
+
+
   return (
     <div className="bg-slate-900 h-screen">
-      <Title /> <br/>
-      <div className="bg-indigo-600">
+      <div className="py-10">
+        <div className="flex justify-center text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-indigo-500 via-indigo-500 to-white">
+          <h1>
+              NU Fintech Leetcode Leaderboard
+          </h1>
+        </div>
+      </div>
+      
+      {ranks.map((rank) => (
+      <div className="bg-gradient-to-r from-cyan-600 to-indigo-500 mb-5 mx-24 rounded-md">
         <div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between flex-wrap">
             <div className="w-0 flex-1 flex items-center">
               <span className="flex p-2 rounded-lg bg-amber-500">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11.049 2.927c.3-.921 1.603-.921 1.902 0l1.519 4.674a1 1 0 00.95.69h4.915c.969 0 1.371 1.24.588 1.81l-3.976 2.888a1 1 0 00-.363 1.118l1.518 4.674c.3.922-.755 1.688-1.538 1.118l-3.976-2.888a1 1 0 00-1.176 0l-3.976 2.888c-.783.57-1.838-.197-1.538-1.118l1.518-4.674a1 1 0 00-.363-1.118l-3.976-2.888c-.784-.57-.38-1.81.588-1.81h4.914a1 1 0 00.951-.69l1.519-4.674z" />
-              </svg>
+                <rank.icon className="h-6 w-6 text-white" aria-hidden="true" />
               </span>
               <p className="ml-3 font-medium text-white truncate">
-                <span className="md:hidden">We announced a new product!</span>
-                <span className="hidden md:inline">#1 {winner}</span>
+                <span className="md:inline">{rank.place}  {rank.name} </span>
+                <span className="md:inline content-center">{rank.count}</span>
+                <span className="hidden md:block"> {rank.description} </span>
               </p>
             </div>
             <div className="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-              <a
-                href="#"
-                className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50"
-              >
-                Profile
-              </a>
+              <span
+                className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-indigo-600 bg-white hover:bg-indigo-50">
+                Rank:
+              </span>
             </div>
             <div className="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
               <button
                 type="button"
                 className="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2"
               >
-
-              
               </button>
+              
             </div>
           </div>
         </div>
+
       </div>
+      ))}
     </div>
   )
 }
@@ -200,6 +251,7 @@ let arr = []
     });
     dict[data.matchedUser.username] = data.matchedUser.submitStats.acSubmissionNum[0].count;
     arr[i] = data.matchedUser.submitStats.acSubmissionNum[0].count;
+    
   }
 
  
@@ -208,7 +260,8 @@ let arr = []
   return {
     props: {
       leaderboard : dict,
-      array : arr
+      array : arr,
+      // easies : data.matchedUser.submitStats.acSubmissionNum[1]
     }
   }
 }
